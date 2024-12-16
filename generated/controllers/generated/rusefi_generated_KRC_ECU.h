8bd7a5c1911e949da4b737f8f9cda5a4b3c54898
GitHub git update Action <action@github.com>
2024-12-16 01:43:01 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KRC_ECU.h
+++ generated/controllers/generated/rusefi_generated_KRC_ECU.h
@@ -1338,7 +1338,7 @@
 #define show_tcu_gauges false
 #define show_test_presets true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3299323396
+#define SIGNATURE_HASH 1806421215
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1800,14 +1800,14 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.12.15.KRC_ECU.3299323396"
+#define TS_SIGNATURE "rusEFI main.2024.12.16.KRC_ECU.1806421215"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
 #define TS_TEST_COMMAND_char t
-#define TS_TOTAL_OUTPUT_SIZE 1804
+#define TS_TOTAL_OUTPUT_SIZE 1800
 #define TS_TRIGGER_SCOPE_CHANNEL_1_NAME "Channel 1"
 #define TS_TRIGGER_SCOPE_CHANNEL_2_NAME "Channel 2"
 #define TS_TRIGGER_SCOPE_DISABLE 5
--- generated/controllers/generated/signature_KRC_ECU.h
+++ generated/controllers/generated/signature_KRC_ECU.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3299323396
-#define TS_SIGNATURE "rusEFI main.2024.12.15.KRC_ECU.3299323396"
+#define SIGNATURE_HASH 1806421215
+#define TS_SIGNATURE "rusEFI main.2024.12.16.KRC_ECU.1806421215"
