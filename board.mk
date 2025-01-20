06e7ddd269b05b8b44fd126a9511ac5fdfa7ce86
Krycholrc <78201348+Krycholrc@users.noreply.github.com>
2025-01-20 22:04:05 +0100

    Update board.mk

--- board.mk
+++ board.mk
@@ -1,15 +1,31 @@
-BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp \
-    $(BOARD_DIR)/default_tune.cpp \
-
-
-BOARDINC += $(BOARD_DIR)/generated/controllers/generated
+# List of all the board related files.
+BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
-# reduce memory usage monitoring
-DDEFS += -DRAM_UNUSED_SIZE=100
+# Override DEFAULT_ENGINE_TYPE
+DDEFS += -DSTM32F407xx
+DDEFS += -DFIRMWARE_ID=\"KRC_ECU\"
+DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
 
-# assign critical LED to a non-existent pin if you do not have it on your board
-# good old PD14 is still the default value
-# DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+# TS_SECONDARY_UxART_PORT
+# DDEFS += -DSTM32_SERIAL_USE_USART3=TRUE 
+# DDEFS += -DTS_SECONDARY_UxART_PORT=SD3 -DEFI_TS_SECONDARY_IS_SERIAL=TRUE
+# DDEFS += -DEFI_AUX_SERIAL=FALSE
+
+# Main Relay control
+DDEFS += -DEFI_MAIN_RELAY_CONTROL=TRUE
+
+# Knock
+DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE
+
+# EGT
+DDEFS += -DEFI_MAX_31855=TRUE
+
+# reduce memory usage monitoring
+DDEFS += -DRAM_UNUSED_SIZE=200
+
+# Critical LED (Err)
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::E8
