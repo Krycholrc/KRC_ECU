67b57b72558774f84c4c0e872fd77f5af9d3c624
Krycholrc <78201348+Krycholrc@users.noreply.github.com>
2025-01-20 22:02:13 +0100

    Update knock_config.h

--- knock_config.h
+++ knock_config.h
@@ -4,13 +4,18 @@
 
 #pragma once
 
-// Knock is on ADC3
+// Define the ADC for Knock
 #define KNOCK_ADC ADCD3
 
-// knock 1 - pin PA3
-#define KNOCK_ADC_CH1 ADC_CHANNEL_IN3
-#define KNOCK_PIN_CH1 Gpio::A3
+// Knock sensor 1 settings - Pin PC0
+#define KNOCK_ADC_CH1 ADC_CHANNEL_IN10
+#define KNOCK_PIN_CH1 Gpio::C0
 
-// Sample rate & time - depends on the exact MCU
+// Knock sensor 2 settings - Pin PC1
+#define KNOCK_HAS_CH2 true
+#define KNOCK_ADC_CH2 ADC_CHANNEL_IN11
+#define KNOCK_PIN_CH2 Gpio::C1
+
+// Set the sample rate & time (varies by MCU)
 #define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
 #define KNOCK_SAMPLE_RATE (STM32_PCLK2 / (4 * (84 + 12)))
