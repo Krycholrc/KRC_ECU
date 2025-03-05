2a734de001f3f0807a7d5e237ca522bfe92c6083
GitHub git update Action <action@github.com>
2025-03-05 01:28:23 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_KRC_ECU.h
+++ generated/controllers/generated/signature_KRC_ECU.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 886837947
-#define TS_SIGNATURE "rusEFI main.2025.03.04.KRC_ECU.886837947"
+#define SIGNATURE_HASH 2932169229
+#define TS_SIGNATURE "rusEFI main.2025.03.05.KRC_ECU.2932169229"
