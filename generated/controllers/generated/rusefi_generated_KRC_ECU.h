cae5c5c0e9c74db00916fdd1c712f939118ff64b
GitHub git update Action <action@github.com>
2025-03-26 01:29:59 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KRC_ECU.h
+++ generated/controllers/generated/rusefi_generated_KRC_ECU.h
@@ -1886,7 +1886,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.03.25.KRC_ECU.2851658418"
+#define TS_SIGNATURE "rusEFI main.2025.03.26.KRC_ECU.2851658418"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
