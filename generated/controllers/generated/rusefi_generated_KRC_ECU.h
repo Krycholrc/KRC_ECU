d21e9ec6c1fc54e1ff3915a4d709ac8ed5b4a6a6
GitHub git update Action <action@github.com>
2025-05-09 01:45:15 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KRC_ECU.h
+++ generated/controllers/generated/rusefi_generated_KRC_ECU.h
@@ -1431,7 +1431,7 @@
 #define show_tcu_gauges false
 #define show_test_presets true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2543413043
+#define SIGNATURE_HASH 520668556
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1936,7 +1936,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.05.08.KRC_ECU.2543413043"
+#define TS_SIGNATURE "rusEFI main.2025.05.09.KRC_ECU.520668556"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_KRC_ECU.h
+++ generated/controllers/generated/signature_KRC_ECU.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2543413043
-#define TS_SIGNATURE "rusEFI main.2025.05.08.KRC_ECU.2543413043"
+#define SIGNATURE_HASH 520668556
+#define TS_SIGNATURE "rusEFI main.2025.05.09.KRC_ECU.520668556"
